70b6887fb49a579fd45ec91c83de5b6e9e7aa6da
lliqi <614124284@qq.com>
2019-12-10 16:43:54 +0800

    Update node.js

--- node.js
+++ node.js
@@ -1,10 +1,18 @@
 var http = require('http');
-var server = http.createServer(function(req,res){
+var server = http.createServer(function(request,response){
+
     res.write('hello world');
     switch(req.url){
         case '/getWeather':
             res.end(JSON.stringify({a:1,b:2}));
             break;
+        case '/user/123' :
+            res.end( fs.readFileSync(__dirname + '/static/user'));
+            break;
+        default:
+            //都没有匹配，则是一个静态文件
+            res.end(fs.readFileSync(__dirname + '/static' + req.url))
+            
     }
 });
 server.listen(9000);
