6eb885d6c21c96a4ed42da01b7e2dda22276cfbd
lliqi <614124284@qq.com>
2019-12-10 16:39:28 +0800

    Update server.js

--- server.js
+++ server.js
@@ -3,12 +3,10 @@ var path = require('path');//处理URL模块
 var fs = require('fs');//读写文件
 var url = require('url'); //解析URL
 
-
+//staticRoot静态路径
 function staticRoot(staticPath, req, res){
-    console.log(staticPath);
-
-    console.log(req.url);
-    var pathObj = url.parse(req.url, true); //解析url，得到一个对象
+  //解析url，得到一个对象
+    var pathObj = url.parse(req.url, true); 
     console.log(pathObj);
 
     //如果URL后没有/index.html，默认会读取index.html
@@ -20,12 +18,10 @@ function staticRoot(staticPath, req, res){
     //得到文件的具体请求路径
     var filePath = path.join(staticPath, pathObj.pathname);
 
-    // var fileContent = fs.readFileSync(filePath,'binary')
-    // res.write(fileContent, 'binary')
-    // res.end()
-
     //异步的方式读取文件
+    //binary二进制读文件
     fs.readFile(filePath, 'binary', function(err, fileContent){
+        //判断是否读到
         if(err){
             console.log('404');
             res.writeHead(404, 'not found');
@@ -40,9 +36,7 @@ function staticRoot(staticPath, req, res){
 
 
 }
-
-console.log(path.join(__dirname, 'static'));
-
+//创建服务器
 var server = http.createServer(function(req, res){
     staticRoot(path.join(__dirname, 'static'), req, res) //__dirname代表当前的目录文件,static当前文件夹名
     //path.join(__dirname, 'static')会自动生成当前文件的绝对路径
